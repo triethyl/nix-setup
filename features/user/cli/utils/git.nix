708164c5d646953895081449cb47eda1ef5ebab7
outremonde <outremonde@vivaldi.net>
2025-06-10 20:39:04 -0400

    changed git email
    Former-commit-id: d919c9e21ae173bf93a10756eb0bbb4c9dac24ca

--- features/user/cli/utils/git.nix
+++ features/user/cli/utils/git.nix
@@ -1,12 +1,16 @@
-{ config, lib, ... }: let
+{
+  config,
+  lib,
+  ...
+}: let
   cfg = config.features.cli.utils.git;
 in {
   options.features.cli.utils.git.enable = lib.mkEnableOption "git";
   config = lib.mkIf cfg.enable {
     programs.git = {
       enable = true;
-      userEmail = "culsans@vivaldi.net";
-      userName = "culsans";
+      userEmail = "triethylammonium@pm.me";
+      userName = "triethyl";
     };
   };
 }
