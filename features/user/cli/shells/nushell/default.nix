ef1839cda4e6af9c664e35ae85d3efd2be1579cd
outremonde <outremonde@vivaldi.net>
2025-06-11 19:14:30 -0400

    fixed projinit command
    Former-commit-id: 77b701490fc1dd2b24aaf9d9a4c3361a269b1162

--- features/user/cli/shells/nushell/default.nix
+++ features/user/cli/shells/nushell/default.nix
@@ -66,7 +66,7 @@ in {
             }
             let path = $path | path expand
             cp -r ${./direnv-project-template}/* $path
-            chmod +w $"($path)/*"
+            chmod +w $"($path)/"
             direnv allow
           }
 
