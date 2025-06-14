5fb489649aed5e79197d94a49c3f919c2c8b6ae5
outremonde <outremonde@vivaldi.net>
2025-06-14 19:33:47 -0400

    added nix package for customneovim
    Former-commit-id: 150905e6c617fcafde4524d41d9a25ea789bcba3

---
+++ pkgs/custom-neovim/default.nix
@@ -0,0 +1,60 @@
+{
+  pkgs,
+  inputs,
+}:
+inputs.mnw.lib.wrap pkgs {
+  neovim = pkgs.neovim-unwrapped;
+
+  plugins = with pkgs.vimPlugins; let
+    tangerine-nvim = pkgs.vimUtils.buildVimPlugin {
+      name = "tangerine-nvim";
+      src = pkgs.fetchFromGithub {
+        owner = "udayvir-singh";
+        repo = "tangerine.nvim";
+        tag = "v2.9";
+        hash = "";
+      };
+    };
+  in {
+    dev.config.pure = ./config;
+    start = [
+      tangerine-nvim # Allow configuration in fennel.
+
+      # Core Plugins
+      mini-nvim # Ton of small modules.
+      nvim-lspconfig # Adds lsp presets.
+      # actions-preview-nvim # Adds a selector for LSP actions.
+      nvim-treesitter.withAllGrammars # All treesitter grammars.
+      # tabby-nvim # Tab bar.
+      # friendly-snippets # Extra snippets.
+
+      # Colorschemes
+      oxocarbon-nvim # IBM Carbon themes.
+      rose-pine # Rose Pine themes.
+      kanagawa-nvim # Kanagawa themes.
+      everforest # Green themes.
+      nordic-nvim # Warm dark nordic theme.
+
+      # Miscellaneous Plugins
+      presence-nvim # Discord RPC for nvim.
+      cellular-automaton-nvim # Fun useless plugin.
+    ];
+    opt = [];
+  };
+
+  luaFiles = [./config/init.lua];
+
+  extraBinPath = with pkgs; [
+    # LSP Servers
+    lua-language-server # Lua LS
+    fennel-ls # Fennel LS
+    nixd # Nix LS
+    marksman # Markdown LS
+
+    # Formatters
+    alejandra # Nix Formatter
+
+    # Extra Tools
+    ripgrep
+  ];
+}
