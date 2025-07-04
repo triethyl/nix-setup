645024746a29a181355d03600cf9eb4534b4d1b7
triethyl <triethylammonium@pm.me>
2025-07-03 21:58:13 -0400

    initialized
    Former-commit-id: b1c7893832fe8cdda2fa72825ed7aaa6244c395f

---
+++ features/user/gui/desktops/niri/parts/quickshell/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Project flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            hello
+          ];
+        };
+      }
+    );
+}
