e44a859b819c000ed43f03bcdf93ba9dd0e34640
triethyl <triethylammonium@pm.me>
2025-07-13 17:25:51 -0400

    added hash for neovide-git package

--- pkgs/neovide-git.nix
+++ pkgs/neovide-git.nix
@@ -5,6 +5,6 @@ pkgs.neovide.overrideAttrs (old: {
     owner = "neovide";
     repo = "neovide";
     rev = "627956990b4d85a2a98a13e0c50da2155d15cd72";
-    sha256 = "";
+    sha256 = "sha256-WwLEL6GyLLPbGEJrpSRLTJr+CqevZZ5MBZNUbXLWn3I=";
   };
 })
