f094608985b80cc279cbc4700b23c9b1381fb3c4
triethyl <triethylammonium@pm.me>
2025-07-29 19:57:20 -0400

    neovim: add ui file

---
+++ pkgs/custom-neovim/config/lua/ui.lua
@@ -0,0 +1,13 @@
+-- UI customizations without plugins.
+
+local statusline = {
+  ' %t',
+  '%r',
+  '%m',
+  '%=',
+  '%{&filetype}',
+  ' %2p%%',
+  ' %3l:%-2c '
+}
+
+vim.o.statusline = table.concat(statusline, '')
