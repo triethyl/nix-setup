ef7c5fdf4e957a78d5b6994abc0fc59ac389304d
triethyl <triethylammonium@pm.me>
2025-08-20 21:28:30 -0400

    fixed dfmodsync package

--- pkgs/dfmodsync.nix
+++ pkgs/dfmodsync.nix
@@ -7,11 +7,14 @@ nu
 
   let workshop_dir = "~/.local/share/Steam/steamapps/workshop/content/975370"
   let df_dir = "~/.local/share/Steam/steamapps/common/Dwarf Fortress/mods"
+  let df_installed_dir = "~/.local/share/Steam/steamapps/common/Dwarf Fortress/data/installed_mods"
 
   let workshop_dir = $workshop_dir | path expand
   let df_dir = $df_dir | path expand
+  let df_installed_dir = $df_installed_dir | path expand
 
   glob $"($df_dir)/*" | each {rm -rf $in ; print $"deleted ($in | path basename)"}
+  glob $"($df_installed_dir)/*" | each {rm -rf $in ; print $"deleted ($in | path basename)"}
   glob $"($workshop_dir)/*" | each {cp -r $in $df_dir; print $"copied ($in | path basename)"}
 
   print "Synced Mods!"
